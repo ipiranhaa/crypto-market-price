bfa0cd9b9d8a5bbf4c5f0f09f4fff0f2bf8722f0
PLa <ipiranhaa@Tanates-MacBook-Pro.local>
2017-09-23 20:11:42 +0700

    📺  Display BTC, ETH and OMG price

--- app.js
+++ app.js
@@ -5,27 +5,61 @@ const path = require('path');
 // src
 const bx = require('./modules/bx.js');
 const bfx = require('./modules/bitfinex.js');
+const _ = require('lodash');
+
+
+let bxCache = {};
+let bfxCache = {};
+let onlineUser = 0;
  
 const server = app.listen(4004, function() {
-    console.log('Listening...');
+  console.log('Listening...');
 });
 
 // socket
 const io = require('socket.io').listen(server);
+
+function updateOnlineUser(oper) {
+  if (oper === '+') {
+    onlineUser++;
+  } else {
+    onlineUser--;
+  }
+  
+  console.log(onlineUser + 'users online');
+  io.emit('online', onlineUser);
+}
+
+function sendCache() {
+  if (!_.isEmpty(bxCache)) {
+    io.emit('bx', bxCache);        
+  }
+
+  if (!_.isEmpty(bfxCache)) {
+    io.emit('bfx', bfxCache);        
+  }
+}
+
 io.on('connection', function(socket) {
   console.log('a user connected');
+  sendCache();
+  updateOnlineUser('+');
 
-  socket.on('chat', function(message) {
-    console.log(message);
+  socket.on('disconnect', function() {
+    updateOnlineUser('-');
   });
 });
 
 bx.fetch(function(data) {
-  console.log('<< BX >>', data);
+  // console.log('<< BX >>', data);
+  bxCache = data;
+  io.emit('bx', data)
 });
 
 bfx.fetch(function(data) {
-  console.log('<< BFX >>', data);
+  // console.log('<< BFX >>', data);
+  bfxCache = data;
+  io.emit('bfx', data)  
 });
  
 app.set('views', path.join(__dirname, 'views'));
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,21 @@
-const socket = io()
+const socket = io();
+
+socket.on('online', function(msg) {
+  console.log(msg + ' users online');
+});
+
+socket.on('bx', function(msg) {
+  console.log('BX: ', msg);
+  msg.forEach(function(obj) {
+    const price = obj.last_price % 1 !== 0 ? obj.last_price.toFixed(2) : obj.last_price;    
+    $('span.bx-' + obj.name.toLowerCase()).html(price);
+  }, this);
+});
+
+socket.on('bfx', function(msg) {
+  console.log('BFX: ', msg);
+  msg.forEach(function(obj) {
+    const price = obj.last_price % 1 !== 0 ? obj.last_price.toFixed(2) : obj.last_price;
+    $('span.bfx-' + obj.name.toLowerCase()).html(price);
+  }, this);
+});
